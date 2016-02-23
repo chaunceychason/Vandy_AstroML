f5806153643758b73522d54f3fd20c3637413b8c
chaunceychason <chaunceychason@gmail.com>
2016-02-23 01:05:00 -0600

    added histograms and statistics

---
+++ profiles/Nick/plotfailguesses.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import numpy as np
+import math
+import matplotlib
+matplotlib.use( 'Agg' )
+import matplotlib.pyplot as plt
+#import sklearn 
+from datetime import datetime
+startTime = datetime.now()
+
+print("WELCOME TO A QUICK PYTHON PROGRAM.")
+print("It will do machine learning stuff with Numbers.. yep. ")
+
+
+"""
+-----------------------------------------------------
+NICHOLAS CHASON 
+Plot False Truth values in histogram to see if theres a pattern.
+-----------------------------------------------------
+
+"""
+
+#f = open("fail_guess_output_file.dat", 'r')
+
+with open("fail_guess_output_file.dat", 'r') as f:
+    lines = f.read().splitlines()
+
+hist_LIST = []
+for i, val in enumerate(lines):
+	hist_LIST.append(int(val))
+
+#print lines
+print("Length of list: %d" % len(hist_LIST))
+
+bin_list = np.linspace(-0.5, 9.5, 11)
+print bin_list
+plt.hist(hist_LIST, bins=bin_list)
+
+
+plt.title("Fail Condition Guess Values")
+plt.xlabel("Guess Value")
+plt.ylabel("Incorrect Guess Count")
+plt.savefig("fail_guess_histogram.png")
+
+print("SAVING FIGURE. fail_guess_histogram.png")
